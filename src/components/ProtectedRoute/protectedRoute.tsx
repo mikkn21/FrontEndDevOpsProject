1b5241df4ffa9633aa606edc8d90e3b4485d40a0
kris098e <abrahamsenkd@gmail.com>
2024-03-13 14:24:41 +0100

    remove redundant

--- src/components/ProtectedRoute/protectedRoute.tsx
+++ src/components/ProtectedRoute/protectedRoute.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { Navigate} from 'react-router-dom';
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 interface ProtectedRouteProps {
-  requireAdmin?: boolean; // allows us to specify whether a page is only for admins 
-  element: React.ReactElement; 
-
+  requireAdmin?: boolean; // allows us to specify whether a page is only for admins
+  element: React.ReactElement;
 }
 
-const ProtectedRoute = ({requireAdmin, element }: ProtectedRouteProps) => {
+const ProtectedRoute = ({ element }: ProtectedRouteProps) => {
   const isAuthenticated = false; // Replace with actual authentication logic
-  const isAdmin = localStorage.getItem('isAdmin') === 'true';
-    
+  const isAdmin = localStorage.getItem("isAdmin") === "true";
 
   // If not authenticated or not ADMIN send to login screen
   if (!isAuthenticated && !isAdmin) {
     return <Navigate to="/login" replace />; // Redirect to login with 'replace' for history
   }
-
 
   return element;
 };
