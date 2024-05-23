d6b053a28cff49419c1fa768331e97e99ad3c03f
B-a-n-g <73177037+B-a-n-g@users.noreply.github.com>
2024-05-24 00:06:42 +0200

    VItest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
+/// <reference types="vitest" />
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vitest/config";
+
+const vitestConfig: UserConfig = {
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: "./src/test/setup.ts"
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,6 +19,8 @@ export default defineConfig(({ mode }) => {
   const isBackendOnline = env.VITE_BACKEND_ONLINE === 'true'; // this is for front-end testing without backend
   console.log(`Backend URL: ${BACKEND_URL}`);
   console.log(`isBackendOnline: ${isBackendOnline}`);
+
+  test: vitestConfig.test
 
   const unavailableBackend = {}
   const proxy = {
